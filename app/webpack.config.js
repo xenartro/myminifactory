95807e594969ac57e2998854212dd1d75fbf8161
Matias Chomicki <matias.chomicki@gmail.com>
2021-09-06 16:17:45 -0300

    Add boostrap, react router, and enable typescript

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/app.tsx')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -55,9 +55,7 @@ Encore
     // enables Sass/SCSS support
     //.enableSassLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
+    .enableTypeScriptLoader()
     .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
