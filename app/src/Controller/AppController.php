dbbf9fd3c845f220cf634dc10ed10da0ee2a7b13
Matias Chomicki <matias.chomicki@gmail.com>
2021-09-06 18:09:53 -0300

    Add endpoint to return the list of users

--- app/src/Controller/AppController.php
+++ app/src/Controller/AppController.php
@@ -1,19 +1,27 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @Route("/app/users", name="app_users_list", methods={"GET"})
      */
     public function users(): JsonResponse
     {
-        $user = $this->getUser();
+        $users = $this->userRepository->getUserList();
 
-        return $this->json([ 'data' => [] ]);
+        return $this->json([ 'data' => $users ]);
     }
 }
